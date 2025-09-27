834c64f9c2994f830fa0978d7f04cffb620ef223
Jahrdin <pirozzi.jordan@gmail.com>
2025-09-27 13:40:39 +0200

    Add form validation (JS + CSS :valid/:invalid)

---
+++ sign-up.js
@@ -0,0 +1,30 @@
+const password = document.getElementById("password");
+const confirm = document.getElementById("confirm");
+const message = document.createElement("p");
+confirm.insertAdjacentElement("afterend", message);
+const form = document.querySelector("form");
+
+function checkPassword(){
+    if(password.value.length > 0 && confirm.value.length > 0){
+    if (password.value !== confirm.value) {
+  confirm.classList.add("error");
+  confirm.classList.remove("success");
+  message.textContent = "❌ Les mots de passe ne correspondent pas";
+  message.className = "error-message"; 
+} else {
+  confirm.classList.add("success");
+  confirm.classList.remove("error");
+  message.textContent = "✅ Les mots de passe correspondent";
+  message.className = "success-message";
+}
+} 
+}
+
+password.addEventListener("input", checkPassword);
+confirm.addEventListener("input", checkPassword);
+
+form.addEventListener("submit", (event) => checkPassword(){
+    if(password.value !== confirm.value){
+     event.preventDefault()   
+    }
+});
